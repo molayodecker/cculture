bf0b75f5a8e3034daf365292dedf8e68426c3307
Arthur Decker <molayodecker@gmail.com>
2020-04-30 00:07:11 -0400

    Added pipenv requirement.txt and setup-eb.sh

---
+++ setup-eb.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+
+mkdir ~/.aws
+touch ~/.aws/config
+chmod 600 ~/.aws/config
+echo "[profile prod]" > ~/.aws/config
+echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> ~/.aws/config
+echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> ~/.aws/config
